88b1293999022a510235b64283e415f1eaa84e8d
Muzietto <marco_faustinelli@yahoo.com>
2015-02-05 14:41:28 +0100

    trying to use restrict

--- directives/js/script.js
+++ directives/js/script.js
@@ -1,11 +1,27 @@
 
-angular.module('directives', [])
-  .controller('Controller', ['$scope',  function($scope){
+angular.module('docsTemplateUrlDirective', [])
+  .controller('Controller', function($scope){
     $scope.customer = {
       name: 'Naomi',
-      address: 'casa sua'
+      address: 'casa sua nello attributello'
     }
-  }])
+  })
   .directive('myCustomer', function(){
     return { templateUrl: 'html/my-customer.html'}
   })
+  
+angular.module('docsRestrictDirective', [])
+  .controller('Controller2', function($scope){
+    $scope.customer = {
+      name: 'Naomi',
+      address: 'casa sua nel tag'
+    }
+  })
+  .directive('myCustomer', function(){
+    return { 
+      restrict: 'E',
+      templateUrl: 'html/my-customer.html'
+    }
+  })
+
+  
