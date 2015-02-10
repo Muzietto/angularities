b86d2599da4a9083df8ea123453847133f775dad
Muzietto <marco_faustinelli@yahoo.com>
2015-02-10 16:05:00 +0100

    a bit of css

--- directives/js/navigation2.js
+++ directives/js/navigation2.js
@@ -1,3 +1,83 @@
+
+
+var navigationApp = angular.module('navigationSandbox', []);
+
+navigationApp.controller('template3Controller', function($scope){
+    $scope.currentCountry = null; // to be chosen in the first select
+    $scope.currentPrice = null; // to be chosen in the second select
+    $scope.currentConnectivity = null; // to be chosen in the third select
+    $scope.currentCreditProvider = null; // to be chosen in the fourth select
+    $scope.countries = blob.countries;
+  })
+  .directive('countryData',function($templateCache){
+    return {
+      restrict: 'E',
+      template: $templateCache.get('country.tmpl'),
+      transclude: true,
+      link: function(scope,element,attrs){
+        scope.$watch(attrs.name,function(value){
+          if (!value) return;
+          repaint();
+        });
+        function repaint(){
+          element.html(template);
+          $compile(element.contents())(scope);
+        }
+      }
+    }
+  })
+  .directive('priceData',function($templateCache){
+    return {
+      restrict: 'E',
+      template: $templateCache.get('price.tmpl'),
+      transclude: true,
+      link: function(scope,element,attrs){
+        scope.$watch(attrs.amount,function(value){
+          if (!value) return;
+          repaint();
+        });
+        function repaint(){
+          element.html(template);
+          $compile(element.contents())(scope);
+        }
+      }
+    }
+  })
+  .directive('connectivityData',function($templateCache){
+    return {
+      restrict: 'E',
+      template: $templateCache.get('connectivity.tmpl'),
+      transclude: true,
+      link: function(scope,element,attrs){
+        scope.$watch(attrs.rid,function(value){
+          if (!value) return;
+          repaint();
+        });
+        function repaint(){
+          element.html(template);
+          $compile(element.contents())(scope);
+        }
+      }
+    }
+  })
+  .directive('creditProviderData',function($templateCache){
+    return {
+      restrict: 'E',
+      template: $templateCache.get('creditProvider.tmpl'),
+      transclude: true,
+      link: function(scope,element,attrs){
+        scope.$watch(attrs.rid,function(value){
+          if (!value) return;
+          repaint();
+        });
+        function repaint(){
+          element.html(template);
+          $compile(element.contents())(scope);
+        }
+      }
+    }
+  })
+
 var blob = {
     "countries": [
         {
@@ -553,88 +633,3 @@ var blob = {
         }
     ]
 }
-
-var navigationApp = angular.module('navigationSandbox', []);
-
-navigationApp.controller('template3Controller', function($scope){
-    $scope.currentCountry = null; // to be chosen in the first select
-    $scope.currentPrice = null; // to be chosen in the second select
-    $scope.currentConnectivity = null; // to be chosen in the third select
-    $scope.currentCreditProvider = null; // to be chosen in the fourth select
-    $scope.countries = blob.countries;
-  })
-  .directive('countryData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('country.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.name,function(value){
-          if (!value) return;
-          $scope.currentPrice = '';
-          $scope.currentConnectivity = '';
-          $scope.currentCreditProvider = '';
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('priceData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('price.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.amount,function(value){
-          if (!value) return;
-          $scope.currentConnectivity = '';
-          $scope.currentCreditProvider = '';
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('connectivityData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('connectivity.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.rid,function(value){
-          if (!value) return;
-          $scope.currentCreditProvider = '';
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('creditProviderData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('creditProvider.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.rid,function(value){
-          if (!value) return;
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-
