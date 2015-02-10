6518f5986d0949ab3d87855f1e8ae81941047d86
Muzietto <marco_faustinelli@yahoo.com>
2015-02-10 16:13:34 +0100

    refactoring

--- directives/js/navigation2.js
+++ directives/js/navigation2.js
@@ -1,82 +1,36 @@
-
-
 var navigationApp = angular.module('navigationSandbox', []);
 
+var directiveFrom = function(templateName,varName){
+  return function($templateCache){
+    return {
+      restrict: 'E',
+      template: $templateCache.get(templateName),
+      transclude: true,
+      link: function(scope,element,attrs){
+        scope.$watch(attrs[varName],function(value){
+          if (!value) return;
+          repaint();
+        });
+        function repaint(){
+          element.html(template);
+          $compile(element.contents())(scope);
+        }
+      }
+    }
+  }
+}
+
 navigationApp.controller('template3Controller', function($scope){
-    $scope.currentCountry = null; // to be chosen in the first select
-    $scope.currentPrice = null; // to be chosen in the second select
-    $scope.currentConnectivity = null; // to be chosen in the third select
-    $scope.currentCreditProvider = null; // to be chosen in the fourth select
+    $scope.currentCountry = null;
+    $scope.currentPrice = null;
+    $scope.currentConnectivity = null;
+    $scope.currentCreditProvider = null;
     $scope.countries = blob.countries;
   })
-  .directive('countryData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('country.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.name,function(value){
-          if (!value) return;
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('priceData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('price.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.amount,function(value){
-          if (!value) return;
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('connectivityData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('connectivity.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.rid,function(value){
-          if (!value) return;
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
-  .directive('creditProviderData',function($templateCache){
-    return {
-      restrict: 'E',
-      template: $templateCache.get('creditProvider.tmpl'),
-      transclude: true,
-      link: function(scope,element,attrs){
-        scope.$watch(attrs.rid,function(value){
-          if (!value) return;
-          repaint();
-        });
-        function repaint(){
-          element.html(template);
-          $compile(element.contents())(scope);
-        }
-      }
-    }
-  })
+  .directive('countryData',directiveFrom('country.tmpl','name'))
+  .directive('priceData',directiveFrom('price.tmpl','amount'))
+  .directive('connectivityData',directiveFrom('connectivity.tmpl','rid'))
+  .directive('creditProviderData',directiveFrom('creditProvider.tmpl','rid'))
 
 var blob = {
     "countries": [
