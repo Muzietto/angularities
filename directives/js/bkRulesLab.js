82a2bc9c68dfe0640208b7c6a2a12d4eba5b1554
Muzietto <marco_faustinelli@yahoo.com>
2016-01-12 20:35:28 +0100

    going strong

--- directives/js/bkRulesLab.js
+++ directives/js/bkRulesLab.js
@@ -1,14 +1,14 @@
 
 angular.module('bkRulesLab', [])
 .controller('bkLabController', function($scope) {
-  $scope.rule = { /* insert here an array of keys */ };
+  $scope.rule = theRule();
   $scope.key = 'payment_volume';
   $scope.value = 'GET IT FROM THE DIRECTIVES';
 })
 .directive('constraintPaymentVolume', function (/*Range*/) {
   return {
     restrict: 'E',
-    replace: true,
+    scope: { key : '=' },
     controller: function ($scope) {
       if (angular.isString($scope.rule.constraints.payment_volume)) {
         $scope.rule.constraints.payment_volume = Range.fromString($scope.rule.constraints.payment_volume);
@@ -17,3 +17,62 @@ angular.module('bkRulesLab', [])
     templateUrl: '../panel/bk/views/constraint/payment_volume.html'
   }
 });
+
+function theRule() {
+  return {
+/*  "priority": 0,
+    "toDelete": false,*/
+    "key": "MERCHANT_REVENUE_SHARE",
+/*    "value": {
+      "revenue-share": [
+        0,
+        "%"
+      ],
+      "applied-to": null
+    },
+    "constraintsKeys": [
+      "payment_method",
+      "country",
+      "credit_provider",
+      "merchant",
+      "payment_volume",
+      "amount",
+      "container_amount",
+      "connectivity",
+      "route",
+      "container_kind",
+      "business_model",
+      "service",
+      "category",
+      "context"
+    ],*/
+    "constraints": {
+      "payment_method": "any",
+      "country": "any",
+      "credit_provider": "any",
+      "merchant": "any",
+      "payment_volume": {
+        "from": {
+          "operator": "",
+          "value": ""
+        },
+        "to": {
+          "operator": "",
+          "value": ""
+        }
+      },
+      "amount": "any",
+      "container_amount": "any",
+      "connectivity": "any",
+      "route": "any",
+      "container_kind": "any",
+      "business_model": "any",
+      "service": "any",
+      "category": "any",
+      "context": "any"
+    }/*,
+    "_id": {
+      "$id": "new-0"
+    }*/
+  }
+}
