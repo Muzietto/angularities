64f511c92e9d2873660f6f33a16aee365c30afd3
Muzietto <marco_faustinelli@yahoo.com>
2016-01-17 20:06:12 +0100

    configuring dashboard main module

--- app/panel/dashboard/js/module.js
+++ app/panel/dashboard/js/module.js
@@ -1,16 +1,20 @@
+
+(function() {
 'use strict';
 
-angular.module('onebip.panel.dashboard', [
-  'onebip.panel.dashboard.directives',
-  'onebip.panel.dashboard.services',
-  'onebip.panel.dashboard.controllers',
-  'ngRoute',
-])
-.config(['$routeProvider', function ($routeProvider) {
+  angular.module('onebip.panel.dashboard', [
+    'onebip.panel.dashboard.directives',
+    'onebip.panel.dashboard.services',
+    'onebip.panel.dashboard.controllers',
+    'ngRoute',
+  ])
+  .config(['$routeProvider', function ($routeProvider) {
 
-  $routeProvider
-    .when('/dashboard/configuration', {
-      templateUrl: '/lib/dashboard/views/configuration.html',
-      controller: 'ConfigurationMain'
-    });
-}]);
+    $routeProvider
+      .when('/dashboard/configuration', {
+        templateUrl: '/dashboard/views/configuration.html',
+        controller: 'ConfigurationMain'
+      });
+  }]);
+
+})();
